87a9f8972c3dba257b7a791a5178e5041181275d
Danny Warren <dannydwarren@gmail.com>
2015-08-07 22:19:48 -0700

    -Captains Log Demo now prompts for voice interation -Captains Log Demo now accepts "Captains Log" then provides a UI to provide a new log entry. -Updated slide deck

--- dev/Cortana_Location/ComputerAssistant.UI/Pages/RecordNotesPage.xaml.cs
+++ dev/Cortana_Location/ComputerAssistant.UI/Pages/RecordNotesPage.xaml.cs
@@ -3,11 +3,18 @@ using System.Linq;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using ComputerAssistant.UI.Models;
+using System;
+using Windows.Media.SpeechSynthesis;
+using Windows.Media.SpeechRecognition;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace ComputerAssistant.UI.Pages
 {
 	public sealed partial class RecordNotesPage : Page
 	{
+		CancellationTokenSource cancellationSource;
 		public RecordNotesPage()
 		{
 			this.InitializeComponent();
@@ -20,6 +27,10 @@ namespace ComputerAssistant.UI.Pages
 		{
 			base.OnNavigatedTo( e );
 
+			cancellationSource = new CancellationTokenSource();
+
+			StartVoiceRecognition();
+
 			if ( !LogEntries.Any() )
 			{
 				//Seed for this run
@@ -30,18 +41,92 @@ namespace ComputerAssistant.UI.Pages
 
 			if ( e.Parameter != null )
 			{
-				AddLogEntry(e.Parameter.ToString());
+				AddLogEntry( e.Parameter.ToString() );
 			}
 
 		}
 
+		SpeechRecognizer speechRecognizerCaptainsLogCommand;
+
+		private async void StartVoiceRecognition()
+		{
+			await SpeakText( "Say Captains Log at any time to create a log entry." );
+
+			speechRecognizerCaptainsLogCommand = new SpeechRecognizer();
+
+			while ( !cancellationSource.IsCancellationRequested )
+			{
+				ISpeechRecognitionConstraint commandConstraint = new SpeechRecognitionListConstraint( new[] { "Captains Log", "Computer Captains Log" } );
+				speechRecognizerCaptainsLogCommand.Constraints.Add( commandConstraint );
+				await speechRecognizerCaptainsLogCommand.CompileConstraintsAsync();
+
+				SpeechRecognitionResult commandResult = await speechRecognizerCaptainsLogCommand.RecognizeAsync();
+
+				if ( commandResult.Status != SpeechRecognitionResultStatus.Success
+					|| commandResult.Confidence == SpeechRecognitionConfidence.Rejected
+					|| cancellationSource.IsCancellationRequested )
+				{
+					continue;
+				}
+
+				var captainsLogDictationRecognizer = new SpeechRecognizer();
+				ISpeechRecognitionConstraint dictationConstraint = new SpeechRecognitionTopicConstraint( SpeechRecognitionScenario.Dictation, "LogEntry", "LogEntryDictation" );
+				captainsLogDictationRecognizer.Constraints.Add( dictationConstraint );
+				await captainsLogDictationRecognizer.CompileConstraintsAsync();
+				captainsLogDictationRecognizer.UIOptions.ExampleText = "Boldly going where no man or woman has gone before.";
+				captainsLogDictationRecognizer.UIOptions.AudiblePrompt = "Go ahead";
+				captainsLogDictationRecognizer.UIOptions.IsReadBackEnabled = true;
+				captainsLogDictationRecognizer.UIOptions.ShowConfirmation = true;
+
+				SpeechRecognitionResult dictationResult = await captainsLogDictationRecognizer.RecognizeWithUIAsync();
+
+				if ( dictationResult.Status != SpeechRecognitionResultStatus.Success
+					|| dictationResult.Confidence == SpeechRecognitionConfidence.Rejected
+					|| string.IsNullOrWhiteSpace( dictationResult.Text )
+					|| cancellationSource.IsCancellationRequested )
+				{
+					captainsLogDictationRecognizer.Dispose();
+
+					continue;
+				}
+
+				AddLogEntry( dictationResult.Text );
+
+				captainsLogDictationRecognizer.Dispose();
+			}
+
+			speechRecognizerCaptainsLogCommand.Dispose();
+		}
+
+		protected override void OnNavigatedFrom( NavigationEventArgs e )
+		{
+			base.OnNavigatedFrom( e );
+
+			cancellationSource.Cancel();
+		}
+
 		private void AddLogEntry( string logEntryText )
 		{
-			if ( string.IsNullOrWhiteSpace(logEntryText) )
+			if ( string.IsNullOrWhiteSpace( logEntryText ) )
 			{
 				return;
 			}
 			LogEntries.Insert( 0, new CaptainsLogEntry( logEntryText ) );
+		}
+
+		private async Task SpeakText( string textToSpeak )
+		{
+			SpeechSynthesizer speechSynth = new SpeechSynthesizer();
+
+			// The media object for controlling and playing audio.
+			MediaElement mediaElement = TextToSpeechMediaElement;
+
+			// Generate the audio stream from plain text.
+			SpeechSynthesisStream stream = await speechSynth.SynthesizeTextToStreamAsync( textToSpeak );
+
+			// Send the stream to the media object.
+			mediaElement.SetSource( stream, stream.ContentType );
+			mediaElement.Play();
 		}
 	}
 }
