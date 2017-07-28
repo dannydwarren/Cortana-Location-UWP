e18f47a40a8f07e781cf2c3152d2504ed2752c82
Danny Warren <dannydwarren@gmail.com>
2017-07-27 22:12:27 -0600

    ComputerAssistant text based bot impl complete

--- dev/Cortana_Location/ComputerAssistant.Bot/Controllers/ComputerController.cs
+++ dev/Cortana_Location/ComputerAssistant.Bot/Controllers/ComputerController.cs
@@ -5,45 +5,98 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System;
+using System.Threading;
 
 namespace ComputerAssistant.Bot.Controllers
 {
     [BotAuthentication]
     public class ComputerController : ApiController
     {
-        /// <summary>
-        /// Endpoint: https://api.cognitive.microsoft.com/sts/v1.0
-        /// //DO NOT COMMIT KEYS!!!!
-        /// Key 1: 47180e9a7bb848c38bfce05a7ada93a7
-        /// 
-        /// Key 2: 049c54f2c9b6441ca257495168246454
-        /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            if (activity.Type == ActivityTypes.Message
+                || activity.Type == ActivityTypes.ConversationUpdate)
             {
-                await Conversation.SendAsync(activity, () => new CaptainsLogDialog());
+                await Conversation.SendAsync(activity, () => new RootDialog());
             }
-           
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
 
         [Serializable]
-        public class CaptainsLogDialog : IDialog<object>
+        public class RootDialog : IDialog<object>
         {
-            public Task StartAsync(IDialogContext context)
+            public async Task StartAsync(IDialogContext context)
             {
+                await context.PostAsync("When you're ready to report say: Captain's Log.");
+
                 context.Wait(MessageReceivedAsync);
-                return Task.CompletedTask;
             }
 
             private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
             {
                 var message = await argument;
-                await context.PostAsync($"Logged: {message.Text}");
+
+                if (!string.IsNullOrWhiteSpace(message.Text)
+                    && (message.Text.ToLower().Contains("captain's log")
+                    || message.Text.ToLower().Contains("captains log")))
+                {
+                    context.Call(new CaptainsLogDialog(), OnResume);
+                }
+                else
+                {
+                    context.Wait(MessageReceivedAsync);
+                }
+            }
+
+            private async Task OnResume(IDialogContext context, IAwaitable<object> result)
+            {
+                var resultFromLog = await result;
+
+                await context.PostAsync($"Your report captain: {resultFromLog}");
+
                 context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        [Serializable]
+        public class CaptainsLogDialog : IDialog<string>
+        {
+            private string _logEntry;
+
+            public async Task StartAsync(IDialogContext context)
+            {
+                await context.PostAsync("Go ahead captain.");
+
+                context.Wait(MessageReceivedAsync);
+            }
+
+            private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+            {
+                var response = await result;
+                _logEntry = _logEntry + " " + response.Text;
+
+                await context.PostAsync($"Your entry so far: {_logEntry}.");
+
+                PromptDialog.Confirm(context, OnResponse, "Would you like to add more?");
+            }
+
+            private async Task OnResponse(IDialogContext context, IAwaitable<bool> result)
+            {
+                var response = await result;
+
+                if (response)
+                {
+                    await context.PostAsync("What would you like to add?");
+
+                    context.Wait(MessageReceivedAsync);
+                }
+                else
+                {
+                    context.Done(_logEntry);
+                }
             }
         }
     }
