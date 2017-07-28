28c49e91b6ff633214f5702dd66d45d20833cdc2
Danny Warren <dannydwarren@gmail.com>
2017-07-27 21:13:24 -0600

    Use actual StarDate

--- dev/Cortana_Location/ComputerAssistant.UI/Models/DateTimeStampedBindable.cs
+++ dev/Cortana_Location/ComputerAssistant.UI/Models/DateTimeStampedBindable.cs
@@ -2,17 +2,35 @@
 
 namespace ComputerAssistant.UI.Models
 {
-	public class DateTimeStampedBindable : Bindable
-	{
-		public DateTimeStampedBindable()
-		{
-			StarDate = DateTime.UtcNow;
-		}
+    public class DateTimeStampedBindable : Bindable
+    {
+        public DateTimeStampedBindable()
+        {
+            StarDate = DateTime.UtcNow;
+        }
 
-		public DateTime StarDate { get; set; }
+        public DateTime StarDate { get; set; }
 
-		public string StardateString => $"{StarDate.ToString( "yyyy" )}.{StarDate.DayOfYear}{StarDate.ToString( "HHmmssff" )}";
+        public string StardateString => $"{StarDate.ToStarDate()}";
 
-		public string CurrentDateString => StarDate.ToLocalTime().ToString( "yyyy-MM-DD HH:mm:ss:ff tt zzz" );
-	}
+        public string CurrentDateString => StarDate.ToLocalTime().ToString("yyyy-MM-DD HH:mm:ss:ff tt zzz");
+    }
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// StarDate by ballance on GitHub: https://github.com/ballance/StarDate
+        /// https://github.com/ballance/StarDate/blob/master/Ballance.StarDate/StarDateConverter.cs</summary>
+        /// <param name="earthDateTime"></param>
+        /// <returns></returns>
+        public static double ToStarDate(this DateTime earthDateTime)
+        {
+            var starDateOrigin = new DateTime(2318, 7, 5, 12, 0, 0);
+            var earthToStarDateDiff = earthDateTime - starDateOrigin;
+            var millisecondConversion = earthToStarDateDiff.TotalMilliseconds / (34367056.4);
+            var starDate = Math.Floor(millisecondConversion * 100) / 100;
+
+            return Math.Round(starDate, 2, MidpointRounding.AwayFromZero);
+        }
+    }
 }
