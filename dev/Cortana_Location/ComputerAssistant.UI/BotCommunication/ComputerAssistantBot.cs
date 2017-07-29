ec09011018c80d18121d890289bd1d6fa2017788
Danny Warren <dannydwarren@gmail.com>
2017-07-28 22:42:03 -0600

    Add ComputerAssistantBot for DirectLine chat bot

---
+++ dev/Cortana_Location/ComputerAssistant.UI/BotCommunication/ComputerAssistantBot.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.Bot.Connector.DirectLine;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ComputerAssistant.UI.BotCommunication
+{
+    public class ComputerAssistantBot
+    {
+        private const string BotId = "computer-assistant";
+        private DirectLineClient _client;
+        private Conversation _conversation;
+        private string _lastWatermark;
+
+        public ComputerAssistantBot()
+        {
+            _client = new DirectLineClient(CredsProvider.DirectLineKey);
+        }
+
+        public async Task<IEnumerable<string>> StartConversation()
+        {
+            _conversation = await _client.Conversations.StartConversationAsync();
+            return await ReceiveMessage();
+        }
+
+        public async Task<IEnumerable<string>> SendMessage(string message)
+        {
+            await _client.Conversations.PostActivityAsync(_conversation.ConversationId, new Activity
+            {
+                Action = ActionTypes.Call,
+                Text = message,
+                From = new ChannelAccount("ComputerAssistantUI")
+            });
+
+            return await ReceiveMessage();
+        }
+
+        private async Task<IEnumerable<string>> ReceiveMessage()
+        {
+            var activitySet = await _client.Conversations.GetActivitiesAsync(_conversation.ConversationId);
+            if (string.IsNullOrWhiteSpace(activitySet.Watermark))
+            {
+                return new List<string>();
+            }
+            _lastWatermark = activitySet.Watermark;
+            var messages = activitySet.Activities.Where(a => a.Id == BotId).Select(a => a.Text).ToList();
+
+            return messages;
+        }
+    }
+}
